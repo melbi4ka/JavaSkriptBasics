19109f8224b48849894f88593aa7e59877a712ae
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-27 12:32:26 +0300

    Add files via upload

---
+++ conditionalStatementAdvanced/summerOutfit.js
@@ -0,0 +1,45 @@
+function summerOutfit (input) {
+    let degrees = Number(input[0]);
+    let timeOfTheDay = input[1];
+    let outfit = "";
+    let shoes = "";
+
+    if (degrees >= 10 && degrees <=18){
+        if (timeOfTheDay === "Morning"){
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+        } else if (timeOfTheDay === "Afternoon"){
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } else if (timeOfTheDay === "Evening"){
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    } else if (degrees > 18 && degrees <= 24){
+        if (timeOfTheDay === "Morning"){
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        } else if (timeOfTheDay === "Afternoon"){
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+        } else if (timeOfTheDay === "Evening"){
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    } else if (degrees >=25){
+        if (timeOfTheDay === "Morning"){
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+        } else if (timeOfTheDay === "Afternoon"){
+            outfit = "Swim Suit"; 
+            shoes = "Barefoot";
+        } else if (timeOfTheDay === "Evening"){
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    }
+    console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`)
+}
+summerOutfit(["22",
+"Afternoon"])
+
