5cfdac6aa804c621b56d5eb89cd9a8000445923c
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-28 12:19:50 +0300

    Add files via upload

---
+++ conditionalStatementAdvanced/cinema.js
@@ -0,0 +1,21 @@
+function cinema (input) {
+    let filmType = input[0];
+    let rows = Number(input[1]);
+    let columns = Number(input[2]);
+
+    let income = rows * columns;
+    if (filmType === "Premiere"){
+        income *= 12;
+    } else if (filmType === "Normal"){
+        income *= 7.50;
+    } else if (filmType === "Discount"){
+        income *= 5;
+    }
+    console.log(`${income.toFixed(2)} leva`)
+}
+cinema(["Discount",
+"12",
+"30"])
+
+
+
