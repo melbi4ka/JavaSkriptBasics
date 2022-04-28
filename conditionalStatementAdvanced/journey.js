7fb6984f98e28df33fd7663975b1013c1fb79905
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-28 12:20:32 +0300

    Add files via upload

---
+++ conditionalStatementAdvanced/journey.js
@@ -0,0 +1,38 @@
+function journey (input){
+    let budget = Number(input[0]);
+    let season = input[1];
+    let typeVacation = "";
+    let destination = ""
+
+
+    switch (season){
+        case "summer":
+            typeVacation = "Camp";
+            break;
+        case "winter":
+            typeVacation = "Hotel";
+            break;
+    }
+    if (budget <= 100){
+        destination = "Bulgaria";
+        if (season === "summer") {
+            budget *= 0.3
+        } else {
+            budget *= 0.7
+        }
+    } else if (budget > 100 && budget <= 1000){
+        destination = "Balkans";
+        if (season === "summer") {
+            budget *= 0.4
+        } else {
+            budget *= 0.8
+        }
+    } else {
+        destination = "Europe";
+        typeVacation = "Hotel";
+        budget *= 0.9
+    }
+    console.log(`Somewhere in ${destination}`)
+    console.log(`${typeVacation} - ${budget.toFixed(2)}`)
+}
+journey(["1500", "summer"])
