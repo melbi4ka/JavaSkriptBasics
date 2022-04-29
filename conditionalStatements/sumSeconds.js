cebb88aa4597f947a689c8d9e09806b2c2b4a207
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-29 11:39:17 +0300

    Add files via upload

---
+++ conditionalStatements/sumSeconds.js
@@ -0,0 +1,24 @@
+function sumSeconds (input) {
+
+    let firstSwimerSec = Number(input[0]);
+    let secondSwimerSec = Number(input[1]);
+    let thirdSwimerSeconds = Number(input[2]);
+
+    let allSwimmerSeconds = firstSwimerSec + secondSwimerSec + thirdSwimerSeconds;
+    let allSwimmerMinutes = Math.floor(allSwimmerSeconds / 60);
+    let allSwimmerTotalSeconds = allSwimmerSeconds % 60;
+
+    if (allSwimmerTotalSeconds >= 10) {
+        console.log(`${allSwimmerMinutes}:${allSwimmerTotalSeconds}`);
+
+    } else {
+        console.log(`${allSwimmerMinutes}:0${allSwimmerTotalSeconds}`);
+
+    }
+   
+} 
+
+sumSeconds(["50",
+"50",
+"49"])
+
