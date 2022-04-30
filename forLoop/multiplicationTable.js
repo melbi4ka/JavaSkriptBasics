98fb4586f05b7d3cab6391b1c0f31886cf86dcc1
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-30 11:02:59 +0300

    Add files via upload

---
+++ forLoop/multiplicationTable.js
@@ -0,0 +1,11 @@
+function multiplicationTable (input){
+    let index = 0;
+    let n = Number(input[index]);
+    index++;
+
+    for (let i=1; i<=10; i++){
+        res = i * n 
+        console.log(i+" * "+n+" = "+res);
+    }
+}
+multiplicationTable(["5"])
