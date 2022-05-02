c519d4849bd038c3128fa9f2ff8a492dc6dd9ca2
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-02 18:03:08 +0300

    Add files via upload

---
+++ forLoop/salarySecond.js
@@ -0,0 +1,42 @@
+function income (input){
+    index = 0;
+    let openTabs = Number(input[index]);
+    index++;
+    let salary = Number(input[index]);
+    index++;
+    let isHaveSalary = true
+
+    for (let i = 0; i < openTabs; i++){
+        let website = input[index];
+        index ++;
+        switch (website){
+            case "Facebook":
+                salary -= 150;
+                break;
+            case "Instagram":
+                salary -= 100;
+                break;
+            case "Reddit":
+                salary -= 50;
+                break;         
+        }
+        if (salary <= 0){
+            isHaveSalary = false
+            console.log("You have lost your salary.")
+            break;
+        }
+    }
+    if (isHaveSalary){
+        console.log(salary)
+    }  
+}
+income(["10",
+"750",
+"Facebook",
+"Dev.bg",
+"Instagram",
+"Facebook",
+"Reddit",
+"Facebook",
+"Facebook"])
+
