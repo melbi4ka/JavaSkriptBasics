b1510d6c0c44a7b84501e2497ab63baaa3ddc186
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-04 10:47:21 +0300

    Add files via upload

---
+++ whileLoop/examPreparation.js
@@ -0,0 +1,50 @@
+function examPreparation (input) {
+    let index = 0;
+    let badGrades = Number(input[index]);
+    index++;
+    
+    let badGradeCounter = 0;
+    let taskCounter = 0;
+    let sumGrades = 0;
+    let lastTask = "";
+    let isEjected = false;
+
+    let taskName = input[index];
+    index++;
+    
+    while (taskName !== "Enough"){
+        let grade = Number(input[index]);
+        index++;
+        if (grade <= 4){
+            badGradeCounter++;
+            if (badGradeCounter === badGrades){
+                isEjected = true;
+                break
+            }
+        }
+        taskCounter++;
+        sumGrades += grade;
+        lastTask = taskName;
+        taskName = input[index];
+        index++;
+    }
+    switch (isEjected){
+        case false:
+            let averageGrade = sumGrades / taskCounter
+            console.log(`Average score: ${averageGrade.toFixed(2)}`)
+            console.log(`Number of problems: ${taskCounter}`)
+            console.log(`Last problem: ${lastTask}`)
+            break;
+        case true:
+            console.log(`You need a break, ${badGradeCounter} poor grades.`)
+            break; 
+    }
+}
+examPreparation(["2",
+"Income",
+"3",
+"Game Info",
+"6",
+"Best Player",
+"4"])
+
